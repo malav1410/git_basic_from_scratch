c8dd1929fdec4bce51de6b0137ddcef9ddb1ec14
Malav Desai <malav1410@gmail.com>
2013-12-14 12:18:30 +0530

    last commit before starting chapter 3

--- hello.rb
+++ hello.rb
@@ -2,3 +2,5 @@ puts "hello"
 puts "2nd hello for 2nd commit"
 puts "2nd commit is nt done yet. but changes have been made to check git diff command!"
 puts "skipping staging area"
+
+puts "chapter 2 is over which have basic git inforation"
